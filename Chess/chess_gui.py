f0b5d6d85f06c39e986e0e050676208d2e726e28
Gregory Eales <gregoeales@aol.com>
2019-02-25 14:15:56 -0600

    GUI optimization

--- Chess/chess_gui.py
+++ Chess/chess_gui.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from chess import Chess
+import time
 
 
 class ChessGUI(object):
@@ -14,12 +15,12 @@ class ChessGUI(object):
 		self.screen = pygame.display.set_mode((size, size))
 		self.piece_paths = self.get_piece_paths()
 		self.piece_images = self.load_images()
+		self.piece_being_held = None
+		self.holding_piece = False
 		pygame.display.set_caption('Chess')
 		self.play_gui()
 
 		
-
-
 
 	def draw_chess_board(self):
 
@@ -48,7 +49,12 @@ class ChessGUI(object):
 				if board[j][i] != '.':
 					self.draw_piece(board[j][i], x=i, y=j)
 
-	def draw_piece(self, symbol, x, y):
+	def draw_piece(self, symbol, x, y, held=False):
+
+		if held == True:
+			self.screen.blit(self.piece_images[symbol], (x, y))
+			return 0
+
 		self.screen.blit(self.piece_images[symbol], (x*int(self.screenX/8), y*int(self.screenX/8)))
 
 	def load_images(self):
@@ -67,25 +73,64 @@ class ChessGUI(object):
 	def play_gui(self):
 		# Variable to keep our main loop running
 		running = True
-
 		# Our main loop!
 		while running:
 		    # for loop through the event queue
+		    pygame.event.wait()
 		    for event in pygame.event.get():
 		        # Check for KEYDOWN event; KEYDOWN is a constant defined in pygame.locals, which we imported earlier
 		        if event.type == KEYDOWN:
 		            # If the Esc key has been pressed set running to false to exit the main loop
 		            if event.key == K_ESCAPE:
 		                running = False
+
+		        if event.type == pygame.MOUSEBUTTONUP:
+		        	pos = pygame.mouse.get_pos()
+		        	self.act_on_piece(pos[0], pos[1])
 		        # Check for QUIT event; if QUIT, set running to false
 		        elif event.type == QUIT:
 		            running = False
 
 		    self.draw_chess_board()
 		    self.draw_pieces(self.sample_board)
+		    if self.holding_piece:
+		    	pos = pygame.mouse.get_pos()
+		    	pos = (pos[0] - self.screenX/16, pos[1] - self.screenX/16)
+		    	self.draw_piece(self.piece_being_held, pos[0], pos[1], held=True)
+	
 		    pygame.display.flip()
 
 
+	def act_on_piece(self, x, y):
+		sectorX = x/(self.screenX/8)
+		sectorY = y/(self.screenY/8)
+		for i in range(9):
+			if sectorX < i:
+				sectorX = int(i - 1)
+				break
+
+		for i in range(9):
+			if sectorY < i:
+				sectorY = int(i-1)
+				break
+
+
+		if self.sample_board[sectorY][sectorX] == "." and not self.holding_piece :
+			pass
+
+
+		elif not self.holding_piece:
+			print("You clicked")
+			self.piece_being_held = self.sample_board[sectorY][sectorX]
+			self.sample_board[sectorY][sectorX] = "."
+			self.holding_piece = True
+
+		elif self.holding_piece:
+			self.sample_board[sectorY][sectorX] = self.piece_being_held
+			self.piece_being_held = None
+			self.holding_piece = False
+
+		
 	def get_piece_paths(self):
 		piece_paths = {}
 		piece_paths["b"] = "chess_assets/b_bishop.png"
