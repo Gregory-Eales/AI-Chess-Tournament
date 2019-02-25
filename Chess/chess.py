0db72664feaec66cddb29537de46795ab6eec755
Gregory Eales <gregoeales@aol.com>
2019-02-25 10:03:24 -0600

    Update chess.py

--- Chess/chess.py
+++ Chess/chess.py
@@ -5,6 +5,7 @@ class Chess(object):
 	def __init__(self):
 
 		self.board = self.reset_board()
+		self.turn = "white"
 
 	# resets the board
 	def reset_board(self):
@@ -23,27 +24,67 @@ class Chess(object):
 		board.append(black_back)
 		return board
 
-
+	# prints the board state
 	def print_board(self):
-		print("-------------------------------------")
+		print("-----------------------------------")
 		for i in range(8):
 
 			for j in range(8):
 				if j == 0:
-					print("|", self.board[i][j], " ", end =" ")
-				if j == 7:
-					print(self.board[i][j], "|")
+					print("| ", self.board[i][j], " ", end =" ")
+				elif j == 7:
+					print(self.board[i][j], " |" , i+1)
 				else:
 					print(self.board[i][j], " ", end =" ")
 
 			if i!=7:
-				print("|                                   |")
+				print("|                                 |")
 
-		print("-------------------------------------")
+		print("-----------------------------------")
+
+		print("   1   2   3   4   5   6   7   8")
+
+	# resets the game
+	def reset_game(self):
+		self.turn = "white"
+		self.board = self.reset_board()
+
 
 	# runs the main game loop
 	def play(self):
-		pass
+
+		# start with whos turn it is
+			# get there move
+			# check if its legal
+			# make move
+			# check if checkmate
+			# change turn
+		
+
+		playing = True
+
+		while playing:
+
+			deciding_turn = True
+
+
+			while deciding_turn:
+
+				move = self.get_player_move()
+
+				if self.check_legal(move):
+					deciding_turn=False
+					self.change_turn()
+
+					
+
+
+			
+
+	# gets the player move irl
+	def get_player_move(self):
+
+
 
 	# returns board state
 	def get_board(self):
@@ -58,8 +99,8 @@ class Chess(object):
 		pass
 
 	# check if move is legal
-	def check_legal(self):
-		pass
+	def check_legal(self, move):
+		return True
 
 	# check if anyones in check
 	def check_if_check(self):
@@ -68,6 +109,12 @@ class Chess(object):
 	# moves piece to location
 	def move_peice(self):
 		pass
+
+	def self.change_turn():
+		if self.turn == "white":
+			self.turn = "black"
+		if self.turn == "black":
+			self.turn = "white"
 
 
 
